303c68747af85777050a79b1d653eed18bd75847
ZJWyrd <Zhloe@JayeWyrd.com>
2024-09-13 10:49:17 +0100

    Tech Task copied into repo

---
+++ Form.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace WF1_Tech_Test_ZJWyrd
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.LoadTab = new System.Windows.Forms.TabPage();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.TableTab = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ChartTab = new System.Windows.Forms.TabPage();
+            this.ReChart = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.LoadTab.SuspendLayout();
+            this.TableTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.ChartTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.LoadTab);
+            this.tabControl1.Controls.Add(this.TableTab);
+            this.tabControl1.Controls.Add(this.ChartTab);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Selecting += new System.Windows.Forms.TabControlCancelEventHandler(this.tabControl_Selecting);
+            // 
+            // LoadTab
+            // 
+            this.LoadTab.Controls.Add(this.label1);
+            this.LoadTab.Controls.Add(this.LoadButton);
+            this.LoadTab.Location = new System.Drawing.Point(4, 22);
+            this.LoadTab.Name = "LoadTab";
+            this.LoadTab.Padding = new System.Windows.Forms.Padding(3);
+            this.LoadTab.Size = new System.Drawing.Size(792, 424);
+            this.LoadTab.TabIndex = 0;
+            this.LoadTab.Text = "Load Data";
+            this.LoadTab.UseVisualStyleBackColor = true;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(332, 201);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 0;
+            this.LoadButton.Text = "Load JSON";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadClick);
+            // 
+            // TableTab
+            // 
+            this.TableTab.Controls.Add(this.dataGridView1);
+            this.TableTab.Enabled = false;
+            this.TableTab.Location = new System.Drawing.Point(4, 22);
+            this.TableTab.Name = "TableTab";
+            this.TableTab.Padding = new System.Windows.Forms.Padding(3);
+            this.TableTab.Size = new System.Drawing.Size(792, 424);
+            this.TableTab.TabIndex = 1;
+            this.TableTab.Text = "Table";
+            this.TableTab.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(786, 418);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ChartTab
+            // 
+            this.ChartTab.Controls.Add(this.ReChart);
+            this.ChartTab.Controls.Add(this.listBox2);
+            this.ChartTab.Controls.Add(this.listBox1);
+            this.ChartTab.Controls.Add(this.chart1);
+            this.ChartTab.Location = new System.Drawing.Point(4, 22);
+            this.ChartTab.Name = "ChartTab";
+            this.ChartTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ChartTab.Size = new System.Drawing.Size(792, 424);
+            this.ChartTab.TabIndex = 2;
+            this.ChartTab.Text = "Chart";
+            this.ChartTab.UseVisualStyleBackColor = true;
+            // 
+            // ReChart
+            // 
+            this.ReChart.Location = new System.Drawing.Point(418, 39);
+            this.ReChart.Name = "ReChart";
+            this.ReChart.Size = new System.Drawing.Size(174, 23);
+            this.ReChart.TabIndex = 3;
+            this.ReChart.Text = "Reload Chart";
+            this.ReChart.UseVisualStyleBackColor = true;
+            this.ReChart.Click += new System.EventHandler(this.ChartLoadClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(134, 6);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(8, 6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(3, 121);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(786, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(301, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose a JSON file to upload";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form";
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.LoadTab.ResumeLayout(false);
+            this.LoadTab.PerformLayout();
+            this.TableTab.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ChartTab.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage LoadTab;
+        private System.Windows.Forms.TabPage TableTab;
+        private System.Windows.Forms.TabPage ChartTab;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button ReChart;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form.cs
@@ -0,0 +1,145 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace WF1_Tech_Test_ZJWyrd
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        DataTable Table;
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void tabControl_Selecting(object sender, TabControlCancelEventArgs e)
+        {
+            if (!e.TabPage.Enabled)
+            {
+                e.Cancel = true;
+                MessageBox.Show("Please load a JSON file first");
+            }
+        }
+
+        private void LoadClick(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Table = new DataTable();
+                try
+                {
+                    string extension = Path.GetExtension(openFileDialog.FileName);
+                    // CSV Doesnt automatically parse to the correct datatype and my attempt here didnt work so ive moved on to just focus on JSON
+                    #region CSV Read/Parse
+                    /*if (extension.ToLower() == ".csv") 
+                    {
+                        using (StreamReader sr = new StreamReader(openFileDialog.FileName))
+                        {
+                            string[] Headers = sr.ReadLine().Split(','); // grab the first line that has all the headers.
+
+                            foreach (string H in Headers)
+                            {
+                                Table.Columns.Add(H);
+                            }
+
+                            while (!sr.EndOfStream)
+                            {
+                                string[] line = sr.ReadLine().Split(',');
+                                DataRow row = Table.NewRow();
+                                for (int i = 0; i < Table.Columns.Count; i++)
+                                {
+                                    if (line[i][0] != '"')
+                                    {
+                                        if (line[i].Contains('.'))
+                                            row[i] = double.Parse(line[i]);
+                                        else 
+                                            row[i] = int.Parse(line[i]);
+                                    }
+                                    else
+                                        row[i] = line[i];
+                                }
+                                Table.Rows.Add(row);
+                            }
+                        }
+
+                    }
+                    else */
+                    #endregion
+                    if (extension.ToLower() == ".json")
+                    {
+                        using (StreamReader sr = new StreamReader(openFileDialog.FileName))
+                        {
+                            Table = (DataTable)JsonConvert.DeserializeObject(sr.ReadToEnd(), (typeof(DataTable)));
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("File not handled. Please use JSON.");
+                    }
+
+                    DataLoaded();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("uh oh. " + ex.Message);
+                }
+            }
+        }
+
+        private void ChartLoadClick(object sender, EventArgs e)
+        {
+            string X = (string)listBox1.SelectedValue;
+            string Y = (string)listBox1.SelectedValue;
+
+            /// ran out of time to make this work but here is the vision
+            /// 1) Select two columns from the two list boxes
+            /// 2) get a new DataTable with only the two chosen columns.
+            /// 3) If necessary, group the Y values based on X values, though this might work naturally
+            /// 4) Based on Things, choose whether bar or scatter graphs are best.
+            ///     - example 1) Lap against Lap Time scatter graph
+            ///     - example 2) Driver ID against Points / Wins Bar Chart
+            ///         - Would need to add multiselect to second listbox 
+            ///     - example 3) Scatter graph of Latitude vs Longitude might look nice
+            /// 5) Some sort of refining to say, only get top 10 drivers from example 2.
+            ///     Another Input for refining in such a way
+            /// 6) export graph as [Format] (Image, Data, etc)
+
+            chart1.Update();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            openFileDialog = new OpenFileDialog();
+        }
+
+        private void DataLoaded()
+        {
+            TableTab.Enabled = true;
+            ChartTab.Enabled = true;
+
+            if (Table.Rows.Count < 500) // arbitrary number
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            else
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+
+            // Need to find a library / package for proper sorting, looking for an excel-esque filter row.
+            dataGridView1.DataSource = Table;
+
+            listBox1.DataSource = Table.Columns.Cast<DataColumn>().Select(x => x.ColumnName).ToList();
+            listBox2.DataSource = Table.Columns.Cast<DataColumn>().Select(x => x.ColumnName).ToList();
+
+            tabControl1.SelectedIndex = 1;
+        }
+    }
+}
